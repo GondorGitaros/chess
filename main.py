bfd1046d784d59ca216c31f23ee6090131d69bd6
GondorGitaros <csaszardaniel81@gmail.com>
2024-01-17 20:10:30 +0100

    update

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ def main():
     # That blabla is the FEN of the starting position
     board = chess.Board("3kr3/R7/2R5/8/P2P2P1/2P4P/2r5/6K1 b - - 0 1")
 
-    try:
-        result = engine.play(board, chess.engine.Limit(time=2.0))
-        board.push(result.move)
-        print("The next move is:", result.move)
-    except chess.engine.EngineError as e:
-        print("EngineError:", e)
+    result = engine.play(board, chess.engine.Limit(time=2.0))
+    board.push(result.move)
+    print("The next move is:", result.move)
 
     engine.quit()
 
