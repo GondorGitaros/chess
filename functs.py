7eace8f82d85a66ef0b0c60e9dca21a41273fb76
GondorGitaros <csaszardaniel81@gmail.com>
2024-01-17 23:22:11 +0100

    deleted file

--- functs.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-import win32api, win32con
-
-def click(x,y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
