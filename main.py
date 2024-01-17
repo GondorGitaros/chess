2f9ec6ef26016904a48645d4d0fdc456bc5507d6
GondorGitaros <csaszardaniel81@gmail.com>
2024-01-17 21:49:59 +0100

    update

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import clipboard
 
 
 
-def show_next_step(FEN):
+def show_next_step(fen):
     engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-windows-x86-64-avx2.exe")
 
+    white = False
     # That blabla is the FEN of the starting position
-    board = chess.Board(FEN)
+    board = chess.Board(fen)
+    for i in range(len(fen)):
+        if fen[i] == " ":
+            if fen[i+1] == "w":
+                white = True
+                break                
 
     result = engine.play(board, chess.engine.Limit(time=2.0))
     board.push(result.move)
-    return result.move
+    return result.move, white
 
     engine.quit()
 
@@ -26,9 +32,9 @@ def main():
     sleep(0.1)
     fen = clipboard.paste()
     print(fen)
-    move = show_next_step(fen)
-    move_from = move[0] + move[1]
-    move_to = move[2] + move[3]
+    move, white = show_next_step(fen)
+    print(move, white)
+    # r2q1rk1/ppp2ppp/5n2/4p3/1b2P3/1P4N1/PP3PPP/R1BQ1K1R b - - 0 1
 
 
 if __name__ == "__main__":
