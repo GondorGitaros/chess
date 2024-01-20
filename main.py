86e27b5fc1e8c858c46c7aa520522401bfd0ffae
GondorGitaros <csaszardaniel81@gmail.com>
2024-01-20 12:17:24 +0100

    working somewhat (there are lil problems but they are solvealbe i think)

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from time import sleep
 from pyautogui import click
 from scan import scan
 
-board = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
+boardhm = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
 
 def show_next_step(fen):
     engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-windows-x86-64-avx2.exe")
-
     white = False
-    # That blabla is the FEN of the starting position
+
     board = chess.Board(fen)
     for i in range(len(fen)):
         if fen[i] == " ":
@@ -19,26 +18,27 @@ def show_next_step(fen):
 
     result = engine.play(board, chess.engine.Limit(time=5))
     board.push(result.move)
-    return result.move, white
+    return result.move
 
     engine.quit()
 
 def main():
     while True:
-        fen = scan()
-        move, white = show_next_step(fen)
+        fen, white  = scan()
+        move = show_next_step(fen)
+        print(move)
         move = str(move)
 
         if white == False:
-            scolumn = 8 - board[move[0]] + 1
+            scolumn = 8 - boardhm[move[0]] + 1
             srow = int(move[1])
-            ecolumn = 8 - board[move[2]] + 1
+            ecolumn = 8 - boardhm[move[2]] + 1
             erow = int(move[3])
         else:
             srow = 8 - int(move[1]) + 1
-            scolumn = board[move[0]]
+            scolumn = boardhm[move[0]]
             erow = 8 - int(move[3]) + 1
-            ecolumn = board[move[2]]
+            ecolumn = boardhm[move[2]]
 
         sleep(0.2)
         click(190 + ((scolumn)*121) - 60, 82 + ((srow)*121) - 60)
--- scan.py
+++ scan.py
@@ -8,6 +8,7 @@ PATH = "D:\\Code\\Git\\Main\\chess\\pieces\\"
 
 
 def scan():
+    white = True
     fen = ""
     replace_colors = [(244, 246, 128), (187, 204, 68), (119, 153, 84), (233, 237, 204)]
     for i in range(8):
@@ -22,29 +23,30 @@ def scan():
             img_path = 'images/' + str(i+1) + str(j+1) + '.png'
             img.save(img_path)
             
-            if pyautogui.locate(PATH + "bb.png", img_path, confidence=0.4):
+            
+            if pyautogui.locate(PATH + "bb.png", img_path, confidence=0.6):
                 fen += "b"
-            elif pyautogui.locate(PATH + "bk.png", img_path, confidence=0.4):
-                fen += "k"
-            elif pyautogui.locate(PATH + "bn.png", img_path, confidence=0.4):
-                fen += "n"
-            elif pyautogui.locate(PATH + "bp.png", img_path, confidence=0.4):
+            elif pyautogui.locate(PATH + "bp.png", img_path, confidence=0.6):
                 fen += "p"
+            elif pyautogui.locate(PATH + "bk.png", img_path, confidence=0.6):
+                fen += "k"
+            elif pyautogui.locate(PATH + "bn.png", img_path, confidence=0.6):
+                fen += "n"
             elif pyautogui.locate(PATH + "bq.png", img_path, confidence=0.4):
                 fen += "q"
             elif pyautogui.locate(PATH + "br.png", img_path, confidence=0.4):
                 fen += "r"
             elif pyautogui.locate(PATH + "wb.png", img_path, confidence=0.4):
                 fen += "B"
-            elif pyautogui.locate(PATH + "wk.png", img_path, confidence=0.4):
-                fen += "K"
-            elif pyautogui.locate(PATH + "wn.png", img_path, confidence=0.4):
-                fen += "N"
-            elif pyautogui.locate(PATH + "wp.png", img_path, confidence=0.4):
+            elif pyautogui.locate(PATH + "wp.png", img_path, confidence=0.35):
                 fen += "P"
-            elif pyautogui.locate(PATH + "wq.png", img_path, confidence=0.4):
+            elif pyautogui.locate(PATH + "wk.png", img_path, confidence=0.35):
+                fen += "K"
+            elif pyautogui.locate(PATH + "wn.png", img_path, confidence=0.35):
+                fen += "N"
+            elif pyautogui.locate(PATH + "wq.png", img_path, confidence=0.35):
                 fen += "Q"
-            elif pyautogui.locate(PATH + "wr.png", img_path, confidence=0.4):
+            elif pyautogui.locate(PATH + "wr.png", img_path, confidence=0.35):
                 fen += "R"    
             else:
                 fen += "1"
@@ -62,19 +64,18 @@ def scan():
 
     # remove the last slash
     fen = fen[:-1]
-    fen += " "
     screen = pyautogui.screenshot()
     # save the image
     screen.save("images/fullpic.png")
     img = Image.open("images/fullpic.png")
     rgb = img.getpixel((1364, 121))
     if rgb == (255, 255, 255):
-        fen += "w "
+        fen += " w - - 0 1"
     else:
-        fen += "b "
+        fen = fen[::-1]
+        fen += " b - - 0 1"
+        white = False
 
     # add the rest of the fen
     #######################
-    return fen
-
-print(scan())
+    return fen, white
