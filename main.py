1caa2058c030403249b1ff0c795b42b5d8634c20
GondorGitaros <csaszardaniel81@gmail.com>
2024-01-26 18:12:32 +0100

    useing someone's scan

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import chess.engine
 from time import sleep
 from pyautogui import click
-from scan import scan
+from scan2 import scan
 
 boardhm = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
 
